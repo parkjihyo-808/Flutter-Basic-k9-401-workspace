d26e73487f58219c773eb652a5f24b8c5f938c0f
lsy3709 <lsy3709@naver.com>
2026-04-01 16:11:23 +0900

    7교시, 미니 프로젝트 화면 작업중, LoginScreen, 입력된 정보 가져와서, 다이얼로그창에 출력 연습2

--- lib/screen/basic2-miniproject/LoginScreen.dart
+++ lib/screen/basic2-miniproject/LoginScreen.dart
@@ -6,6 +6,10 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 1. 입력 값을 제어하는 컨트롤러 선언.
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: const Text('로그인 화면')),
       body: SafeArea(
@@ -15,12 +19,48 @@ class LoginScreen extends StatelessWidget {
             children: [
               const Center(child: FlutterLogo(size: 100)),
               const SizedBox(height: 16),
-              const TextField(decoration: InputDecoration(labelText: '이메일')),
+              // 2, 컨트롤러 적용하기.
+              TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: '이메일')
+              ),
               const SizedBox(height: 16),
-              const TextField(decoration: InputDecoration(labelText: '패스워드')),
+              // 2, 컨트롤러 적용하기.
+              TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(labelText: '패스워드')
+              ),
               const SizedBox(height: 20),
+              // 3. 버튼 클릭시, 이벤트 리스너 동작.
               ElevatedButton(
-                onPressed: () => Navigator.pushNamed(context, '/details'),
+                // 화면 이동하는 이벤트 리스너 , 잠시 대기.
+                // onPressed: () => Navigator.pushNamed(context, '/details'),
+                onPressed:() {
+                  // 다이얼로그창 을 이용하기.
+                  // 화면 중앙에 다이얼로그 뛰우기 작업.
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('가입 정보 확인'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min, // 내용만큼만 높이 차지
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('이메일: ${emailController.text}'),
+                            Text('패스워드: ${passwordController.text}'), // 마스킹 없이 실제값 출력됨
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context), // 창 닫기
+                            child: const Text('확인'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
                 child: const Text('로그인'),
               ),
             ],
